b2ae52113fb5ce34971545169fce696c6017d598
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-22 15:59:04 +0530

    Finished Arrow Functions. Starting with objects and classes.

---
+++ JavaScript Refresher/arrowFunctions.js
@@ -0,0 +1,14 @@
+// Arrow Functions:
+
+// () =>  --- anonymous functions
+
+//normal
+// export default function() {
+//     console.log("Hello!");
+// }
+
+//arrow ... can include parameters
+export default (name) => {
+    console.log(`Hello, ${name}!`);
+}
+
