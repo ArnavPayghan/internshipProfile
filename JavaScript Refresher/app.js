dd6c0b86c1c4f3b2b51de45289609a3f756ca633
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-22 14:02:25 +0530

    import export, file rename for organization.

--- JavaScript Refresher/app.js
+++
@@ -1,8 +0,0 @@
-import { api_key } from "./util.js";
-import randomString from "./util.js"; // default import can be named anything.
-import { abc, def } from "./util.js";
-
-console.log(api_key);
-console.log(randomString);
-console.log(abc);
-console.log(def);
