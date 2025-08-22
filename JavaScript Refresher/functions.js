69d1fca16f0f72fd8626bf0185f09282776c9ab8
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-22 14:43:11 +0530

    Functions and First Problem Solved.

--- JavaScript Refresher/functions.js
+++ JavaScript Refresher/functions.js
@@ -3,3 +3,30 @@ function greet() {
 }
 
 greet();
+
+// parameters
+
+function greetPerson(name, message) {
+    console.log("Hello, " + name + "! " + message);
+}
+
+greetPerson("Alice", "How are you?");
+greetPerson("Bob", "Nice to meet you!");
+
+// default parameters
+
+function greetGuest(name = "Guest", message = "Welcome!") {
+    console.log("Hello, " + name + "! " + message);
+}
+
+greetGuest();
+greetGuest(name = "Alice", message = "How are you?"); // CAN BE OVERWRITTEN
+
+// Using return
+
+function greetingMethod(name, message) {
+    return "Hello, " + name + "! " + message;
+}
+const greeting1 = greetingMethod("Dev", "Welcome to the team!");
+console.log(greeting1);
+console.log(greetingMethod("Dev", "Welcome to the team! - consoleLog"));
