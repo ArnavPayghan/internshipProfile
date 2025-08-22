90751b63ef73f62980cef230edfade9511a623a6
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-22 19:25:43 +0530

    arrays functions.

--- JavaScript Refresher/arrays.js
+++ JavaScript Refresher/arrays.js
@@ -1,2 +1,19 @@
 // Arrays & Array Methods like map()
 
+const hobbies = ["Reading", "Traveling", "Gaming"];
+
+console.log(hobbies);
+console.log(hobbies[0]);
+
+// .push()
+hobbies.push("Cooking");
+console.log(hobbies);
+
+// .findIndex(() => {})
+const index = hobbies.findIndex((item) => {
+    return item === "Gaming";
+});
+// const index = hobbies.findIndex((item) => item === "Gaming"); --- Shorter Version.
+console.log(index);
+
+
