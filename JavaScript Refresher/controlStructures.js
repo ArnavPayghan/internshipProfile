b8c42d113ec4245dccbb017551e821e6d7868099
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-23 10:52:37 +0530

    Started control structs.

---
+++ JavaScript Refresher/controlStructures.js
@@ -0,0 +1,9 @@
+// Control Structures
+
+const password = prompt("Enter your password:");
+
+if (password === "0001") {
+  console.log("Access granted.");
+} else {
+  console.log("Access denied.");
+}
