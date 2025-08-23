228f274be276c301987914723b082303b4a92bc6
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-23 11:30:40 +0530

    Manipulating DOM understood, but still bit confused on how react helps with this.

---
+++ JavaScript Refresher/manipulatingDOM.js
@@ -0,0 +1,15 @@
+// DOM Manipulation using JavaScript
+
+// Something we wont be doing is reaching out to the website and manipulate the DOM manually.
+// But it is a strength of JS to do it in runtime.
+// We won't be doing it in React either, because React will do it for us.
+// Because we'll write React code in a declarative way.
+
+const list = document.querySelector('ul');
+list.remove(); // removes the ul element from the DOM.
+
+
+// My research notes on DOM:
+
+// basically a programming interface that lets you interact with and manipulate the structure of a web page.
+// tree-like representation of everything in an HTML (or XML) document.
