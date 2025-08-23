44d3a4f4dd42eaaab8c6ae0dec4770294c2e8e45
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-23 10:56:22 +0530

    Finished Control Structs.

--- JavaScript Refresher/controlStructures.js
+++ JavaScript Refresher/controlStructures.js
@@ -1,9 +1,18 @@
 // Control Structures
 
-const password = prompt("Enter your password:");
+// IF Statements.
+const password = prompt("Enter your password:"); // won't work in VS Code, needs browser.
 
 if (password === "0001") {
   console.log("Access granted.");
 } else {
   console.log("Access denied.");
 }
+
+// FOR Loops.
+const hobbies = ["Reading", "Gaming", "Hiking"];
+
+for (const hobby of hobbies) {
+  console.log(`I enjoy ${hobby}.`);
+}
+
