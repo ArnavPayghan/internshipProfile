ad0c8fde8d4b6e4705fc24be38163a78901b1133
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-25 22:25:54 +0530

    No need for JSX.

--- S2 - React Essentials/starting-project/src/App.jsx
+++ S2 - React Essentials/starting-project/src/App.jsx
@@ -37,15 +37,9 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {/* here title, description and image are PROPS */}
-            <CoreConcept 
-              title="Component" 
-              description="Core UI building block." 
-              image={componentsImg} />
-            <CoreConcept 
-              title={CORE_CONCEPTS[1].title} 
-              description={CORE_CONCEPTS[1].description} 
-              image={CORE_CONCEPTS[1].image} />
-            <CoreConcept { ...CORE_CONCEPTS[2] } />
+            {CORE_CONCEPTS.map(conceptItem => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
