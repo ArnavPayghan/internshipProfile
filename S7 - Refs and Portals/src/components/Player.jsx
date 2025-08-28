80d415f9388e5e97d41c6c89b315b74ef933aee2
ArnavPayghan <arnavvv.p@gmail.com>
2025-08-28 23:44:08 +0530

    Name set

--- S7 - Refs and Portals/src/components/Player.jsx
+++ S7 - Refs and Portals/src/components/Player.jsx
@@ -16,7 +16,7 @@ export default function Player() {
     <section id="player">
       <h2>Welcome { submitted ? enteredPlayerName : "unknown entity" }</h2>
       <p>
-        <input type="text" onClick={handleChange} value={enteredPlayerName} />
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
